0155a7f5b0b70729592b3b4cf810d59d5989e823
azeljkovic <aleksandar.zeljkovic91@gmail.com>
2026-06-02 16:47:24 +0200

    optimize exports

---
+++ src/assertions/index.ts
@@ -0,0 +1,3 @@
+export * from "./body.js";
+export * from "./headers.js";
+export * from "./response.js";
--- test/unit/exports.test.js
+++ test/unit/exports.test.js
@@ -1,11 +1,97 @@
+import assert from "node:assert/strict";
 import { describe, it } from "node:test";
-import { lightyAssert } from "../../dist/index.js";
+import * as lighty from "../../dist/index.js";
 import { makeResult } from "../helpers/responses.js";
 
 describe("namespace export", () => {
-  it("exposes helpers through lighty", () => {
-    lightyAssert.responseIsOk(makeResult(200));
-    lightyAssert.statusCodeIs(makeResult(204), 204);
-    lightyAssert.bodyEquals({ id: 1 }, { id: 1 });
+  it("exposes request helpers through direct and namespace exports", () => {
+    const requestExportNames = [
+      "createClient",
+      "deleteRequest",
+      "getRequest",
+      "headRequest",
+      "HttpRequestError",
+      "InvalidJsonResponseError",
+      "optionsRequest",
+      "patchRequest",
+      "postRequest",
+      "putRequest",
+      "request",
+    ];
+
+    for (const exportName of requestExportNames) {
+      assert.equal(typeof lighty[exportName], "function");
+      assert.equal(lighty.lightyRequest[exportName], lighty[exportName]);
+    }
+
+    assert.deepEqual(
+      Object.keys(lighty.lightyRequest).sort(),
+      requestExportNames.sort(),
+    );
+  });
+
+  it("exposes assertion helpers through direct and namespace exports", () => {
+    const assertionExportNames = [
+      "bodyArrayContains",
+      "bodyArrayContainsItemMatching",
+      "bodyArrayIsEmpty",
+      "bodyArrayIsNotEmpty",
+      "bodyArrayLengthIs",
+      "bodyContains",
+      "bodyEquals",
+      "bodyHasLength",
+      "bodyHasProperty",
+      "bodyIncludesProperties",
+      "bodyIsArray",
+      "bodyIsEmpty",
+      "bodyIsNoContent",
+      "bodyLengthIsAtLeast",
+      "bodyLengthIsGreaterThan",
+      "bodyMatches",
+      "bodyObjectIsEmpty",
+      "bodyPathEquals",
+      "contentTypeIsJson",
+      "headerExists",
+      "headerIncludes",
+      "headerIs",
+      "headerMatches",
+      "headerSatisfies",
+      "responseIsAccepted",
+      "responseIsBadRequest",
+      "responseIsClientError",
+      "responseIsConflict",
+      "responseIsCreated",
+      "responseIsForbidden",
+      "responseIsNoContent",
+      "responseIsNotFound",
+      "responseIsOk",
+      "responseIsRedirect",
+      "responseIsServerError",
+      "responseIsSuccessful",
+      "responseIsTooManyRequests",
+      "responseIsUnauthorized",
+      "responseIsUnprocessableEntity",
+      "statusCodeIs",
+      "statusCodeIs2xx",
+      "statusCodeIs3xx",
+      "statusCodeIs4xx",
+      "statusCodeIs5xx",
+      "statusCodeIsInRange",
+      "statusCodeIsOneOf",
+    ];
+
+    for (const exportName of assertionExportNames) {
+      assert.equal(typeof lighty[exportName], "function");
+      assert.equal(lighty.lightyAssert[exportName], lighty[exportName]);
+    }
+
+    assert.deepEqual(
+      Object.keys(lighty.lightyAssert).sort(),
+      assertionExportNames.sort(),
+    );
+
+    lighty.lightyAssert.responseIsOk(makeResult(200));
+    lighty.lightyAssert.statusCodeIs(makeResult(204), 204);
+    lighty.lightyAssert.bodyEquals({ id: 1 }, { id: 1 });
   });
 });
